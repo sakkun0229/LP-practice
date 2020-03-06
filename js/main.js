baeb998afb738791331d708893810d11c240e7b3
Takahisa Sakuma <tsheadbang@gamil.com>
2020-03-06 13:59:27 +0900

    add navbar responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("hello from main.js")
+
+const navSlide = () => {
+  const nav = document.getElementById('navLinks')
+  const burger = document.getElementById('navBurger')
+  const overlay = document.getElementById('navOverlay')
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('navbar__items--active')
+    burger.classList.toggle('navbar__burger--active')
+    overlay.classList.toggle('navbar__overlay--active')
+  })
+
+  overlay.addEventListener('click', () => {
+    nav.classList.toggle('navbar__items--active')
+    burger.classList.toggle('navbar__burger--active')
+    overlay.classList.toggle('navbar__overlay--active')
+  })
+}
+
+navSlide()
